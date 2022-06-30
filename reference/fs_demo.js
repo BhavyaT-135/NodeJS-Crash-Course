d9aa26d378a2cd40031dac6e2eec48685b17f819
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-07-01 02:08:03 +0530

    ✨ Using the fs module

---
+++ reference/fs_demo.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+
+//Create folder
+
+// fs.mkdir(path.join(__dirname, 'test'), {}, err => {
+//     if (err) throw err;
+//     console.log('Folder created...');
+// });
+ 
+//Create and write to file (async method)
+
+// fs.writeFile(path.join(__dirname, 'test', 'hello.txt'), 'Hello World!', err => {
+//     if (err) throw err;
+//     console.log('File created and written...');
+//     fs.appendFile(path.join(__dirname, 'test', 'hello.txt'), 'I Love NodeJS!', err => {
+//     if (err) throw err;
+//     console.log('File appended...');
+//     });
+// });
+
+//Read file
+
+// fs.readFile(path.join(__dirname, 'test', 'hello.txt'), 'utf8', (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+// });
+ 
+//Rename file
+
+fs.rename(path.join(__dirname, 'test', 'hello.txt'), path.join(__dirname, 'test', 'helloWorld.txt'), (err, data) => {
+    if (err) throw err;
+    console.log('File renamed...');
+});
