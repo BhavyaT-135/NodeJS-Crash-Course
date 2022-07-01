1a8d266578239bbc66b4795bb193f227c19a06a0
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-07-01 13:47:13 +0530

    ✨ Using the http module

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-const Logger = require('./logger');
-const logger = new Logger();
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-logger.on('message', (data) => {
-    console.log('Called Listener:', data);
-})
-
-logger.log('Hello World!');
